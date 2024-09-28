e0eebb12fa5ccf210427215a3b1705006578a539
Boaz Sze <boaz.sze@gmail.com>
2024-09-29 04:12:18 +0800

    fix

--- bots_tts_read_chat.py
+++ bots_tts_read_chat.py
@@ -12,8 +12,7 @@ def read_file(file_path):
         content = file.read()
         chats = re.split(r"\[chat#\d+\]\[\w+\]:\n", content)
         chats = [chat for chat in chats]
-        paired_chats = [chats[i] + chats[i + 1] for i in range(0, len(chats) - 1, 2)]
-    return paired_chats
+    return chats
 
 
 def main():
@@ -23,13 +22,13 @@ def main():
     counter = 0
     file_to_read = input("Enter the .txt file to read (sampleresponse.txt): ")
     while True:
-        i = counter % 2 if counter != 0 else 0
         counter += 1
+        i = counter % 2
         engine.setProperty("voice", voices[i].id)
 
         chats = read_file(file_to_read)
         cleaned_response = re.sub(r"\[chat#\d+\]AI\[\d+\]:", "", chats[counter]).strip()
-        print(f"[chat#{counter}]: {cleaned_response}")
+        print(f"\n[chat#{counter}]: {cleaned_response}")
         text_to_speech(engine, cleaned_response)
 
 
